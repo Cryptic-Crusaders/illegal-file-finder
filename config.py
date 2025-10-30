f300ab4c14e0bc15effd4a80105ddbf118e2d8bf
realendie <enderprooffical@gmail.com>
2025-10-30 18:25:39 -0400

    Refactor folder path and formatting in config.py

--- config.py
+++ config.py
@@ -1,27 +1,26 @@
 from pathlib import Path
 from itertools import chain
 
-FOLDER_PATH = Path('drive:/folder/another-folder/yet-another-folder')
+FOLDER_PATH = Path("test")
 # FOLDER_PATH = Path('D:/Downloads')
 
 SUBFOLDER_NAME_TO_EXTENSIONS = {
-    'video': ('mp4', 'mov', 'avi', 'mkv', 'wmv', 'mpg', 'mpeg', 'm4v', 'h264'),
-    'audio': ('mp3', 'wav', 'ogg', 'flac', 'aif', 'mid', 'midi', 'wma'),
-    'image': ('jpg', 'png', 'bmp', 'jpeg', 'svg', 'tif', 'tiff'),
-    'archive': ('zip', 'rar', '7z', 'z', 'gz', 'pkg', 'deb'),
-    'text': ('pdf', 'txt', 'doc', 'docx', 'rtf', 'odt'),
-    'spreadsheet': ('xlsx', 'xls', 'xlsm'),
-    'presentation': ('pptx', 'ppt'),
-    'book': ('fb2', 'epub', 'mobi'),
-    'gif': ('gif',),
+    "video": ("mp4", "mov", "avi", "mkv", "wmv", "mpg", "mpeg", "m4v", "h264"),
+    "audio": ("mp3", "wav", "ogg", "flac", "aif", "mid", "midi", "wma"),
+    "image": ("jpg", "png", "bmp", "jpeg", "svg", "tif", "tiff"),
+    "archive": ("zip", "rar", "7z", "z", "gz", "pkg", "deb"),
+    "text": ("pdf", "txt", "doc", "docx", "rtf", "odt"),
+    "spreadsheet": ("xlsx", "xls", "xlsm"),
+    "presentation": ("pptx", "ppt"),
+    "book": ("fb2", "epub", "mobi"),
+    "gif": ("gif",),
     # 'subfolder-name': ('extension', 'another-extension')
 }
 
-LOGS_FOLDER = Path('logs')
+LOGS_FOLDER = Path("logs")
 
 SUBFOLDER_NAMES = tuple(SUBFOLDER_NAME_TO_EXTENSIONS.keys())
-EXTENSIONS = tuple(chain.from_iterable(
-    (SUBFOLDER_NAME_TO_EXTENSIONS.values())))
+EXTENSIONS = tuple(chain.from_iterable((SUBFOLDER_NAME_TO_EXTENSIONS.values())))
 
 
 def get_subfolder_name_by_extension(extension: str) -> str:
