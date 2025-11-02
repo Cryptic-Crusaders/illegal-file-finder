98e0c3b526b07f54087acc8dcf41c62acde05b37
Wyatt Johnson <enderprooffical@gmail.com>
2025-11-01 22:18:09 -0400

    Removed unused subfolders

--- config.py
+++ config.py
@@ -8,10 +8,6 @@ SUBFOLDER_NAME_TO_EXTENSIONS = {
     "video": ("mp4", "mov", "avi", "mkv", "wmv", "mpg", "mpeg", "m4v", "h264"),
     "audio": ("mp3", "wav", "ogg", "flac", "aif", "mid", "midi", "wma"),
     "image": ("jpg", "png", "bmp", "jpeg", "svg", "tif", "tiff"),
-    "archive": ("zip", "rar", "7z", "z", "gz", "pkg", "deb"),
-    "text": ("pdf", "txt", "doc", "docx", "rtf", "odt"),
-    "spreadsheet": ("xlsx", "xls", "xlsm"),
-    "presentation": ("pptx", "ppt"),
     "book": ("fb2", "epub", "mobi"),
     "gif": ("gif",),
     # 'subfolder-name': ('extension', 'another-extension')
